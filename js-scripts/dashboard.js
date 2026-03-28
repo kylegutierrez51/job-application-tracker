53000522bfda9391820f3a43e6afad4c70f730a0
kyle <kylegutierrez51@gmail.com>
2026-03-28 04:25:21 -0400

    Hardcode dashboard.js to update width for each bar.

--- js-scripts/dashboard.js
+++ js-scripts/dashboard.js
@@ -33,4 +33,74 @@ function renderSummaryCards() {
   document.querySelector('.js-summary-cards').innerHTML = summaryCards;
 }
 
+function renderAppPipeline() {
+  
+  /*
+    TODO: Create a function that takes the statuses of the jobs (COUNT()), divides by total number of jobs, and updates the bar size (width) based on the percentage
+
+    The function can return an array of numbers for each percentage
+  */
+
+
+
+
+  let appPipeline = `
+    <div class="app-pipeline-container">
+      <p>Application Pipeline</p>
+      <div class="pipeline-layout">
+
+        <div class="app">
+          <div class="app-details">
+            <div>Applied</div>
+            <div>${3}</div>
+          </div>
+          <div id="applied-bar" class="applied-bar"></div>
+        </div>
+
+        <div class="app">
+          <div class="app-details">
+            <div>Phone Screen</div>
+            <div>${2}</div>
+          </div>
+          <div id="phone-bar" class="phone-bar"></div>
+        </div>
+        
+        <div class="app">
+          <div class="app-details">
+            <div>Interview Scheduled</div>
+            <div>1</div>
+          </div>
+          <div id="interview-bar" class="interview-bar"></div>
+        </div>
+
+        <div class="app">
+          <div class="app-details">
+            <div>Offer</div>
+            <div>1</div>
+          </div>
+          <div id="offer-bar" class="offer-bar"></div>
+        </div>
+
+        <div class="app">
+          <div class="app-details">
+            <div>Rejected</div>
+            <div>1</div>
+          </div>
+          <div id="rejected-bar" class="rejected-bar"></div>
+        </div>
+
+      </div>
+    </div>
+  `;
+
+  document.querySelector('.js-app-pipeline').innerHTML = appPipeline;
+
+  document.getElementById('rejected-bar').style.width = '75%';
+  document.getElementById('offer-bar').style.width = '10%';
+  document.getElementById('interview-bar').style.width = '75%';
+  document.getElementById('phone-bar').style.width = '67%';
+  document.getElementById('applied-bar').style.width = '90%';
+}
+
 renderSummaryCards();
+renderAppPipeline();
