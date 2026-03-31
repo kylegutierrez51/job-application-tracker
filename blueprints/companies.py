33dbdbdf78bc5922e20e606777c2ca24a219d89d
kyle <kylegutierrez51@gmail.com>
2026-03-31 18:54:54 -0400

    Create CRUD routes for companies

--- blueprints/companies.py
+++ blueprints/companies.py
@@ -1,10 +1,46 @@
-from flask import Blueprint, render_template, current_app, jsonify
+from flask import Blueprint, render_template, current_app, request, jsonify
 
 companies_bp = Blueprint("companies", __name__)
 
-@companies_bp.route("/")
+
+def serialize_company(company):
+    return {
+        **company,
+        'created_at': company['created_at'].isoformat() if company['created_at'] is not None else None,
+    }
+
+
+@companies_bp.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("companies.html")
+    db = current_app.db
+
+    if request.method == 'POST':
+        # VALIDATION LOGIC
+        data = request.get_json()
+        db.add_job(data)
+        return jsonify({'success': True})
+
+    if request.method == 'GET':
+        companies = [serialize_company(c) for c in db.get_all_companies()]
+        return render_template("companies.html", companies=companies)
+
+
+
+@companies_bp.route("/<int:company_id>", methods=["PUT", "DELETE"])
+def modify_job(company_id):
+    db = current_app.db
+
+    if request.method == 'PUT':
+        # VALIDATION LOGIC
+        data = request.get_json()
+        db.edit_company(data, company_id)
+        print('Edited Company: ', db.get_company(company_id), '\n')
+        return jsonify({'success': True})
+
+    if request.method == 'DELETE':
+        db.delete_company(company_id)
+        return jsonify({'success': True})
+
 
 
 
